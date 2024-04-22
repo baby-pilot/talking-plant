124833f7db42ab1f0f7f0c1df5fd9e91528911e1
jo6109072 <john.pan@mail.mcgill.ca>
2024-04-21 22:37:37 -0400

    add pygame for automatic read-out

--- bt_speak.py
+++ bt_speak.py
@@ -3,7 +3,18 @@ Implements speaking capability for the plant via bluetooth
 '''
 
 from gtts import gTTS
+import pygaame
+import time
+
+pygame.init()
+
+
 tts = gTTS("hello, I am raspberry pi 4. This is a test with Google Text to speech (gTTS)")
 tts.save("hello.mp3")
+alert_sound = pygame.mixer.Sound("hello.mp3")
+alert_sound.play()
+
+# wait for sound to finish playing
+time.sleep(alert_sound.get_length())
 
 # use mpg123 on rasp pi to play the file above
