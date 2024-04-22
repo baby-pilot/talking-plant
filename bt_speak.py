41cac1784de2faae916aef61b7460fe59600144c
jo6109072 <john.pan@mail.mcgill.ca>
2024-04-21 22:06:38 -0400

    add gtts

---
+++ bt_speak.py
@@ -0,0 +1,7 @@
+'''
+Implements speaking capability for the plant via bluetooth
+'''
+
+from gtts import gTTS
+tts = gTTS("hello, I am raspberry pi 4. This is a test with Google Text to speech (gTTS)")
+tts.save("hello.mp3")
