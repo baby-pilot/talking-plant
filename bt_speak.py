fa5cf45fb83bea6c8add3098ac4a25a00fc836a8
jo6109072 <john.pan@mail.mcgill.ca>
2024-04-21 22:45:07 -0400

    use playsound

--- bt_speak.py
+++ bt_speak.py
@@ -3,18 +3,20 @@ Implements speaking capability for the plant via bluetooth
 '''
 
 from gtts import gTTS
-import pygaame
+# import pygaame
 import time
+from playsound import playsound
 
 pygame.init()
 
 
 tts = gTTS("hello, I am raspberry pi 4. This is a test with Google Text to speech (gTTS)")
 tts.save("hello.mp3")
-alert_sound = pygame.mixer.Sound("hello.mp3")
-alert_sound.play()
+playsound("./hello.mp3")
+# alert_sound = pygame.mixer.Sound("hello.mp3")
+# alert_sound.play()
 
-# wait for sound to finish playing
-time.sleep(alert_sound.get_length())
+# # wait for sound to finish playing
+# time.sleep(alert_sound.get_length())
 
 # use mpg123 on rasp pi to play the file above
