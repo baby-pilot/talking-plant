60ac4e97b26e9a27d912536784c859dc190ac31b
jo6109072 <john.pan@mail.mcgill.ca>
2024-04-23 22:21:34 -0400

    feat: add moisture check and scripts

--- bt_speak.py
+++ bt_speak.py
@@ -3,20 +3,32 @@ Implements speaking capability for the plant via bluetooth
 '''
 
 from gtts import gTTS
+from enum import Enum
 import time
 import vlc
 from datetime import datetime
 
+## --------------------------------------  Generate scripts  ---------------------------------------------
 tts = gTTS("hello, I am raspberry pi 4. This is a test with Google Text to speech (gTTS)")
 tts.save("hello.mp3")
-# setup VLC player
-player = vlc.MediaPlayer("hello.mp3")
-player.play()
-while player.get_state() in [vlc.State.Playing, vlc.State.Opening, vlc.State.Buffering]:
-    time.sleep(1)  # check every second whether audio is still playing
 
-player.stop()  # Stop player when playback complete
+water_alert = gTTS("Hey this is your plant speaking, please water me.")
+water_alert.save("water_alert.mp3")
+
+uv_alert = gTTS("Hey, it's me again, can you please move me, I need some sunshine.")
+uv_alert.save("uv_alert.mp3")
+
+defense_alert = gTTS("It's me, plant, you gotta defend me, I'll be eaten!")
+defense_alert.save("defense_alert.mp3")
+
+## ------------------------------------------------------------------------------------------------------
+
 # can use mpg123 on rasp pi to play the file above manually from command line
+class AlertMode(Enum):
+    NEED_WATER = "water_alert.mp3"
+    NEED_UV = "uv_alert.mp3"
+    NEED_DEFENSE = "defense_alert.mp3"
+
 
 def generateAndPlay(message) -> None:
     filename = f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.mp3"
@@ -31,3 +43,14 @@ def playFromFile(filename) -> None:
         time.sleep(1)  # check every second whether audio is still playing
     player.stop()  # Stop player when playback complete
     # can use mpg123 on rasp pi to play the file above manually from command line
+
+def speak(mode: AlertMode):
+    audio_file = mode.value
+    # setup VLC player
+    player = vlc.MediaPlayer(audio_file)
+    player.play()
+    while player.get_state() in [vlc.State.Playing, vlc.State.Opening, vlc.State.Buffering]:
+        time.sleep(1)  # check every second whether audio is still playing
+
+    player.stop()  # Stop player when playback complete
+# can use mpg123 on rasp pi to play the file above manually from command line
---
+++ moisture_check.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+from .send_sms import send_txt
+from .bt_speak import speak, AlertMode
+
+# Set up GPIO pin
+sensor_pin = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(sensor_pin, GPIO.IN, initial=GPIO.LOW)
+
+try:
+    while True:
+        if GPIO.input(sensor_pin) == GPIO.LOW:
+            # Soil is dry! Need watering.
+            speak(AlertMode.NEED_WATER)
+        else:
+            print("Checked soil. Is wet, no need watering.")
+        time.sleep(10)
+except KeyboardInterrupt:
+    # clean up resources used to avoid damage to RPi
+    GPIO.cleanup()
